4de55d6fbb720439e41ac5043c38fd290075028f
andrea.cavalher <andrea.cavalher@BRSPMDNO15309>
2016-07-28 17:56:04 -0300

    #1

---
+++ ProjetctTest/ProjetctTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetctTest.Startup))]
+namespace ProjetctTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
